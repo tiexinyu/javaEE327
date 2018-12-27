84937852c4b67bb3b9e154e65842cf6f99b039a3
123 <123@itcast>
2018-12-27 17:40:22 +0800

    1

---
+++ ssm_Dao/src/main/java/com/itheima/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.itheima.mapper;
+
+import com.itheima.pojo.Product;
+
+import java.util.List;
+
+public interface ProductMapper {
+
+    List<Product> queryProductList() throws Exception;
+
+
+}
